0e290476560da8eb8e22330b6aaf60afb0a8683d
bykbaiev <oleksandr.bykbaiev@youscan.io>
2020-12-10 23:09:43 +0200

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                loader: 'file-loader?name=[name].[ext]'
+            },
+            {
+                test: /\.elm$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                use: {
+                    loader: 'elm-webpack-loader',
+                    options: {}
+                }
+            }
+        ]
+    },
+
+    devServer: {
+        inline: true,
+        stats: 'errors-only'
+    }
+};
